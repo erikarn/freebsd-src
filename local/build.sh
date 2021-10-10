1cc27ed82c0e0b5f14ee997ac624948b72980c38
Adrian Chadd <adrian@freebsd.org>
2021-10-10 09:52:24 -0700

    [local] Use llvm12
    llvm10 doesn't link things right and the linker sets just don't work
    right at all.  This is why i was seeing early openfirmware init failures;
    it wasn't finding the ofw_fdt provider.

--- local/build.sh
+++ local/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-# env MAKEOBJDIRPREFIX=/home/adrian/work/freebsd/head-embedded-arm/freebsd-obj make -j4 buildworld buildkernel KERNCONF=ASUS_AC1300 CROSS_TOOLCHAIN=llvm10 TARGET_ARCH=armv7 NO_CLEAN=1 SRCCONF=../src.conf MODULES_OVERRIDE="" UBLDR_LOADADDR='0x80208000"
-env MAKEOBJDIRPREFIX=/home/adrian/work/freebsd/head-embedded-arm/freebsd-obj make -j4 buildkernel KERNCONF=ASUS_AC1300 CROSS_TOOLCHAIN=llvm10 TARGET_ARCH=armv7 NO_CLEAN=1 SRCCONF=../src.conf MODULES_OVERRIDE="" UBLDR_LOADADDR="0x80208000"
+TCHAIN=llvm12
+
+# env MAKEOBJDIRPREFIX=/home/adrian/work/freebsd/head-embedded-arm/freebsd-obj make -j4 buildworld buildkernel KERNCONF=ASUS_AC1300 CROSS_TOOLCHAIN=${TCHAIN} TARGET_ARCH=armv7 NO_CLEAN=1 SRCCONF=../src.conf MODULES_OVERRIDE="" UBLDR_LOADADDR='0x80208000"
+env MAKEOBJDIRPREFIX=/home/adrian/work/freebsd/head-embedded-arm/freebsd-obj make -j4 buildkernel KERNCONF=ASUS_AC1300 CROSS_TOOLCHAIN=${TCHAIN} TARGET_ARCH=armv7 NO_CLEAN=1 SRCCONF=../src.conf MODULES_OVERRIDE="" UBLDR_LOADADDR="0x80208000"
