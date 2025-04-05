bc5b4686cd4eae5e90b9414465931a1653dde3d2
Adrian Chadd <adrian@FreeBSD.org>
2025-04-05 13:53:36 -0700

    local: some local build scripts

---
+++ local/build-world.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+. ./opts.inc
+
+cd ${X_SRCDIR} || exit 1
+
+env MAKEOBJDIRPREFIX=${X_OBJDIR} make ${X_MAKEOPTS} KERNCONF=ASUS_AC1300 \
+    TARGET_ARCH=armv7 NO_CLEAN=1 ${X_CROSS_TOOLCHAIN_ENTRY} \
+    SRCCONF=src.conf MODULES_OVERRIDE="" UBLDR_LOADADDR='0x80208000' \
+    SRC_ENV_CONF=src-env.conf buildworld
