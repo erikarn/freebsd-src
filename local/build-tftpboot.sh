b58cf3725c0fc000273ce91391cb22f795a2fc07
Adrian Chadd <adrian@FreeBSD.org>
2025-05-27 12:40:57 -0700

    local: fix building dtb; twiddle with building a FIT again
    still doesn't boot, but at least it builds

--- local/build-tftpboot.sh
+++ local/build-tftpboot.sh
@@ -5,17 +5,21 @@
 cd ${X_SRCDIR} || exit 1
 
 #echo "*** Making dtb"
-#sys/tools/fdt/make_dtb.sh /home/adrian/work/freebsd/head-embedded-arm/freebsd-src/sys /usr/home/adrian/work/freebsd/head-embedded-arm/freebsd-src/sys/dts/arm/qcom-ipq4018-rt-ac58u.dts ..
+env MACHINE=arm sys/tools/fdt/make_dtb.sh ${X_PREFIX}/freebsd-src/sys ${X_PREFIX}/freebsd-src/sys/dts/arm/qcom-ipq4018-rt-ac58u.dts ..
 
 echo "*** Copying kernel"
 cp ${X_OBJDIR}/data/1/adrian/freebsd/head-embedded-arm/freebsd-src/arm.armv7/sys/ASUS_AC1300/kernel ../kernel.elf
 cp ${X_OBJDIR}/data/1/adrian/freebsd/head-embedded-arm/freebsd-src/arm.armv7/sys/ASUS_AC1300/kernel.debug ../kernel.debug
 
+# TODO: this doesn't work; I have a lot more stuff to do for the DTB!
+echo "*** empty initrd"
+dd if=/dev/zero of=../ramdisk.bin bs=1024 count=64
+
 echo "*** Building FIT"
-# mkimage -f local/test.its ../test.itb
+mkimage -f local/test.its ../test.itb
 
 echo "*** Copying into /tftpboot"
-# cp -vf ../test.itb /tftpboot
+cp -vf ../test.itb /tftpboot
 cp -vf ../kernel.elf /tftpboot
-#cp -vf ../qcom-ipq4018-rt-ac58u.dtb /tftpboot/kernel.dtb
-#
+cp -vf ../qcom-ipq4018-rt-ac58u.dtb /tftpboot/kernel.dtb
+
