erikarn/freebsd-src

commit 2023566223a4020d832bd4e25dc7dadfd660b836

[net80211] fix NULL pointer dereference in VHT operation in hostap mode.
The vht IEs are NULL at this point, so we shouldn't upgrade a node to VHT.
I'll fix the upgrade after this!

Tested:

* ath10k, hostap mode

sys/net80211/ieee80211_hostap.c
@@ -2151,7 +2151,9 @@ hostap_recv_mgmt(struct ieee80211_node *ni, struct mbuf *m0,
 		ni->ni_chan = vap->iv_bss->ni_chan;
 
 		/* VHT */
-		if (IEEE80211_IS_CHAN_VHT(ni->ni_chan)) {
+		if (IEEE80211_IS_CHAN_VHT(ni->ni_chan) &&
+		    htcap != NULL &&
+		    vhtinfo != NULL) {
 			/* XXX TODO; see below */
 			printf("%s: VHT TODO!\n", __func__);
 			ieee80211_vht_node_init(ni);

commit 9f2fceadc0981b5ea254080d4043f316dbf9e952

net80211: fix VHT node setup in hostap mode
Way back in 2017 I wrote the initial VHT hostap support, and then
added a NULL check here to stop it from panicing.  However, there
is no VHTINFO IE in an ASSOC_REQ management frame.  Thus, the node
would never have VHT state initialised.

This mirrors what HT node setup does (which again doesn't have
HTINFO IE's in an ASSOC_REQ frame), the upgrade will happen once
the exchange completes.

Fixes: 2023566223a4020d832bd4e25dc7dadfd660b836
Differential Revision:	https://reviews.freebsd.org/D50787

sys/net80211/ieee80211_hostap.c
@@ -2214,12 +2214,9 @@ hostap_recv_mgmt(struct ieee80211_node *ni, struct mbuf *m0,
 
 		/* VHT */
 		if (IEEE80211_IS_CHAN_VHT(ni->ni_chan) &&
-		    vhtcap != NULL &&
-		    vhtinfo != NULL) {
-			/* XXX TODO; see below */
-			net80211_vap_printf(vap, "%s: VHT TODO!\n", __func__);
+		    vhtcap != NULL) {
 			ieee80211_vht_node_init(ni);
-			ieee80211_vht_update_cap(ni, vhtcap, vhtinfo);
+			ieee80211_vht_update_cap(ni, vhtcap);
 		} else if (ni->ni_flags & IEEE80211_NODE_VHT)
 			ieee80211_vht_node_cleanup(ni);
 

sys/net80211/ieee80211_vht.c
@@ -838,12 +838,10 @@ ieee80211_add_vhtinfo(uint8_t *frm, struct ieee80211_node *ni)
 }
 
 void
-ieee80211_vht_update_cap(struct ieee80211_node *ni, const uint8_t *vhtcap_ie,
-    const uint8_t *vhtop_ie)
+ieee80211_vht_update_cap(struct ieee80211_node *ni, const uint8_t *vhtcap_ie)
 {
 
 	ieee80211_parse_vhtcap(ni, vhtcap_ie);
-	ieee80211_parse_vhtopmode(ni, vhtop_ie);
 }
 
 static struct ieee80211_channel *

sys/net80211/ieee80211_vht.h
@@ -52,8 +52,7 @@ uint8_t *	ieee80211_add_vhtinfo(uint8_t *frm, struct ieee80211_node *);
 uint8_t *ieee80211_add_vhtcap_ch(uint8_t *, struct ieee80211vap *,
     struct ieee80211_channel *);
 
-void	ieee80211_vht_update_cap(struct ieee80211_node *,
-	    const uint8_t *, const uint8_t *);
+void	ieee80211_vht_update_cap(struct ieee80211_node *, const uint8_t *);
 
 struct ieee80211_channel *
 	ieee80211_vht_adjust_channel(struct ieee80211com *,