1a228c2bab61fa9dea44453cd53e82b0ce967b91
Adrian Chadd <adrian@FreeBSD.org>
2025-12-14 19:45:36 -0800

    net80211: clean up time comparison in HT code
    There's at least one place where the time comparison is wrong, leading
    to some bad beahaviour around deciding to try and establish/reestablish
    AMPDU TX sessions.
    
    Start addressing these!

--- sys/net80211/ieee80211_ht.c
+++ sys/net80211/ieee80211_ht.c
@@ -1130,6 +1130,8 @@ again:
 		 */
 		if (rap->rxa_qframes != 0) {
 			/* XXX honor batimeout? */
+
+			/* XXX time comparison macro? */
 			if (ticks - rap->rxa_age > ieee80211_ampdu_age) {
 				/*
 				 * Too long since we received the first
@@ -1392,6 +1394,7 @@ ieee80211_ht_node_age(struct ieee80211_node *ni)
 		 * See above for more details on what's happening here.
 		 */
 		/* XXX honor batimeout? */
+		/* XXX time comparison macro? */
 		if (ticks - rap->rxa_age > ieee80211_ampdu_age) {
 			/*
 			 * Too long since we received the first
@@ -2819,7 +2822,8 @@ ieee80211_ampdu_request(struct ieee80211_node *ni,
 		/* defer next try so we don't slam the driver with requests */
 		tap->txa_attempts = ieee80211_addba_maxtries;
 		/* NB: check in case driver wants to override */
-		if (tap->txa_nextrequest <= ticks)
+		/* XXX TODO: verify this is the correct ticks math */
+		if (ieee80211_time_before_eq(tap->txa_nextrequest, ticks))
 			tap->txa_nextrequest = ticks + ieee80211_addba_backoff;
 		return 0;
 	}
