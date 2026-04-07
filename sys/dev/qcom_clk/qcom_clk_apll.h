14875e1d044b31424dec04f4b4f74cd8dd3cfe81
Adrian Chadd <adrian@FreeBSD.org>
2026-04-07 02:43:48 -0700

    qcom_clk: add initial APLL placeholder clock node
    I don't know what I'm doing here yet; there's a lot of variations of this
    clock node in linux and I'd hate to implement it by copy/paste one node
    into like a couple dozen variants.
    
    In any case the initial goal is to stand up the root level gcc PLL
    clocks (which are already programmed and all i have to deal with is
    gate/ungate and read frequency) and see what they look like.
    
    And I don't yet know what to do with the second type I need to implement -
    the apll fixed div clock, why's Linux put it in the same driver?
    The apssdiv driver for IPQ401x did something similar and I ended up
    just changing my clock tree to instead use a fixed PLL clock and then
    a fixed divisor clock with the right name/DT ID for the rest of them.

---
+++ sys/dev/qcom_clk/qcom_clk_apll.h
@@ -0,0 +1,59 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2026 Adrian Chadd <adrian@FreeBSD.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef	__QCOM_CLK_APLL_H__
+#define	__QCOM_CLK_APLL_H__
+
+typedef enum {
+	QCOM_CLK_APLL_TYPE_FIXED_LUCID_EVO = 1,
+	QCOM_CLK_APLL_TYPE_POSTDIV_LUCID_EVO = 2,
+	QCOM_CLK_APLL_TYPE_LUCID_EVO = 3,
+} qcom_clk_apll_type_t;
+
+struct qcom_clk_apll_def {
+	struct clknode_init_def clkdef;
+	qcom_clk_apll_type_t apll_type;
+	uint32_t enable_offset;
+	uint32_t enable_shift;
+};
+
+#define F_APLL_LUCID_OLE_FIXEDDIV(_id, _cname, _parent, _eoffset, _eshift)	\
+{									\
+	.clkdef.id = _id,						\
+	.clkdef.name = _cname,						\
+	.clkdef.parent_names = (const char *[]){_parent},		\
+	.clkdef.parent_cnt = 1,						\
+	.clkdef.flags = CLK_NODE_STATIC_STRINGS,			\
+	.enable_offset = _eoffset,					\
+	.enable_shift = _eshift,					\
+	.apll_type = QCOM_CLK_APLL_TYPE_FIXED_LUCID_EVO,		\
+}
+
+extern	int qcom_clk_apll_register(struct clkdom *clkdom,
+	    struct qcom_clk_apll_def *clkdef);
+
+#endif	/* __QCOM_CLK_APLL_H__ */
