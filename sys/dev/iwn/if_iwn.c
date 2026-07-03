854991e45cd3e0535b4a1ac1ff170bba2d694563
Adrian Chadd <adrian@FreeBSD.org>
2026-07-03 16:15:46 -0700

    iwn: fix iwn to assign seqno in raw path
    oops

--- sys/dev/iwn/if_iwn.c
+++ sys/dev/iwn/if_iwn.c
@@ -4600,6 +4600,7 @@ iwn_tx_data(struct iwn_softc *sc, struct mbuf *m, struct ieee80211_node *ni)
 	/* Only assign if not A-MPDU; the A-MPDU TX path will do its own */
 	if ((m->m_flags & M_AMPDU_MPDU) == 0)
 		ieee80211_output_seqno_assign(ni, -1, m);
+	DPRINTF(sc, IWN_DEBUG_XMIT, "%s: seqno=0x%x\n", __func__, le16toh(*((uint16_t *) &wh->i_seq[0])));
 
 	/* Encrypt the frame if need be. */
 	if (wh->i_fc[1] & IEEE80211_FC1_PROTECTED) {
@@ -6443,6 +6444,7 @@ iwn_check_rx_recovery(struct iwn_softc *sc, struct iwn_stats *rs)
 	    (delta_msec + delta_cck + delta_ofdm + delta_ht),
 	    thresh);
 
+#if 0
 	/*
 	 * If we need a retune, then schedule a single channel scan
 	 * to a channel that isn't the currently active one!
@@ -6450,6 +6452,8 @@ iwn_check_rx_recovery(struct iwn_softc *sc, struct iwn_stats *rs)
 	 * The math from linux iwlwifi:
 	 *
 	 * if ((delta * 100 / msecs) > threshold)
+	 *
+	 * TODO: amusingly this scan scheduling was never enabled! lol.
 	 */
 	if (thresh > 0 && (delta_cck + delta_ofdm + delta_ht) * 100 > thresh) {
 		DPRINTF(sc, IWN_DEBUG_ANY,
@@ -6460,6 +6464,7 @@ iwn_check_rx_recovery(struct iwn_softc *sc, struct iwn_stats *rs)
 		    (delta_cck + delta_ofdm + delta_ht) * 100,
 		    thresh);
 	}
+#endif
 }
 
 /*
